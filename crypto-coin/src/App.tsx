3fe3df7b505de8a43d5800eddc1ccf874ba5fd51
Shaid Azmin <excel.azmin@gmail.com>
2025-07-29 16:57:00 +0600

    add two project

---
+++ crypto-coin/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Vite + React + Tailwind CSS   
+      </h1>
+    </>
+  )
+}
+
+export default App
